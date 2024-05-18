48bba4682ce81b5594e3ad7145e9a7ddc701fc10
magomedov-m <magomedov.dgu@gmail.com>
2024-05-18 21:36:37 +0300

    Добавил новые стили для более красивого отображения иконки "Избранное".

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import Main from "./components/main";
 import { Routes, Route, Link } from "react-router-dom";
 import Favourites from "./components/favourites";
 import IconFav from "./components/IconFav";
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
 
 
 
 
 function App() {
+  const headerRef = useRef(null);
+  const [showFavourites, setShowFavourites] = useState(true);
+
+
   let [orders, setOrders] = useState([]);
   
   const addToOrder = (item) => {
@@ -27,17 +31,37 @@ function App() {
     setOrders(orders.filter(el => el.id !== id))
   }
 
+  // Отвечает за скрытие и всплытие иконки "Избранное"
+  useEffect(() => {
+    const handleScroll = () => {
+      const headerHeight = headerRef.current.offsetHeight;
+      const scrollPosition = window.pageYOffset;
+
+      if (scrollPosition > headerHeight) {
+        setShowFavourites(false);
+      } else {
+        setShowFavourites(true);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <>
-
-      <header>
-        <Link to="/favourites"><IconFav/></Link>
+      <header ref={headerRef}>
+        {/* <Link to="/favourites"><IconFav/></Link> */}
+        {showFavourites && <Link to="/favourites"><IconFav/></Link>}
       </header>
       <Routes>
         <Route path="/" element={<Main addToOrder={addToOrder} orders={orders} />}/>
         <Route path="/favourites" element={<Favourites orders={orders} onDelete={deleteOrder} />} />
       </Routes>
-      </>
+    </>
   );
 }
 
