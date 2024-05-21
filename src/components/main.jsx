8ce6f1378c790e95bd17758463feed3c36577099
magomedov-m <magomedov.dgu@gmail.com>
2024-05-22 00:17:42 +0300

    Fetch-запрос
    Внес дополнения в Fetch-запрос. Использую другую базу данных.

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -7,68 +7,41 @@ import Items from "./Items";
 
 
 function Main(props) {
+  let [arrFilms, setArrFilms] = useState([]);
+
+  const API_KEY = 'c032e2d7';
+  const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}`;
+
+  let data;
+  arrFilms = data;
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data);
+  }
+
   // const API_KEY = 'https://api.kinopoisk.dev/v1.4/movie?year=2023&genres.name=все';
   // const headers = {
   //   'X-API-KEY': '1DAP24W-ASD4WA3-N0R7Q2W-3T3KNFT'
   // };
-  let [arrFilms, setArrFilms] = useState([]);
-  
-
-  arrFilms = [
-    {'name': 'С приветом по планетам', id: 1, 'imdb': 10},
-    {'name': 'Близняшки', id: 2, 'imdb': 10},
-    {'name': 'Подлые наглецы', id: 3, 'imdb': 10},
-    {'name': 'Гиганты', id: 4, 'imdb': 10},
-    {'name': 'Неотвратимые', id: 5, 'imdb': 10}
-  ];
   
   useEffect(() => {
-    console.log(props.orders)
-  }, [props.orders]);
+    searchMovies()
+  }, []);
+
+  
+  
+  // useEffect(() => {
+  //   console.log(props.orders)
+  // }, [props.orders]);
 
   const addToOrder = (item) => {
     props.addToOrder(item)
   }
 
-  // useEffect(() => {
-  //   async function getFilms() {
-      // try {
-        // const response = await fetch(API_KEY, { headers: headers });
-        // if (!response.ok) {
-          // throw new Error(`HTTP error! status: ${response.status}`);
-        // }
-        // const movies = await response.json();
-        // setArrFilms(movies.docs);
-      // } catch (error) {
-        // console.log('Упс... Кажется, ', error);
-      // }
-  //   };
-  // }, []);
-
-  // const [arrFilms, setArrFilms] = useState([]);
-
-  // useEffect(() => {
-  //   getFilms();
-  // }, []);
-  //   const btnSearch = document.querySelector('.search')
-  //   const inputSearch = document.querySelector('.icons__search-input')
-  //   const filmCard = document.querySelector('.film-card')
-  // let arrFilms;
-  // async function getFilms() {
-
-  //   let url = 'https://api.kinopoisk.dev/v1.4/movie?year=2023&genres.name=все';
-  //   let resp = await fetch(`${url}`, {
-  //       headers: {
-  //           'X-API-KEY': '1DAP24W-ASD4WA3-N0R7Q2W-3T3KNFT'
-  //       }
-  //   })
-
-  //   let filmsJson = await resp.json()
-  //   console.log(filmsJson)
-    // setArrFilms(filmsJson.docs)
-  // }
-
-  // getFilms()
+  
 
   return (
       <div className="container">
