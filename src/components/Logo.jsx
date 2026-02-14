bccf1d61573c6464b4d7f8400cdfdf9d7db4fce8
magomedov-m <magomedov.dgu@gmail.com>
2026-02-14 22:55:43 +0300

    рефактор: удалил ненужные файлы и папки

--- src/components/Logo.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import logo from '../assets/img/KinoPoisk-Logo.wine.png'
-
-export default function Logo() {
-  return (
-    <div className='logo'><img src={logo} alt='Логотип' /></div>
-  )
-}
