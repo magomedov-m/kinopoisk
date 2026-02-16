ec278a96b08f2605ef0eb80bb828b2122f87e3d9
magomedov-m <magomedov.dgu@gmail.com>
2026-02-16 18:11:14 +0300

    логика: исправил функцию для удаления карточки из избранного

--- src/entities/movie/MovieFavouriteCard.jsx
+++ src/entities/movie/MovieFavouriteCard.jsx
@@ -2,14 +2,14 @@ import { MdDelete } from "react-icons/md";
 import { useFavourites } from "../../features/favourites/context";
 
 export default function MovieFavouriteCard({ movie }) {
-  const { remove } = useFavourites();
+  const { removeFromFavourites } = useFavourites();
 
   return (
     <div className="movieCard">
       <img src={movie.poster?.previewUrl} alt="" />
       <h3>{movie.alternativeName}</h3>
 
-      <button onClick={() => remove(movie.id)}>
+      <button onClick={() => removeFromFavourites(movie.id)}>
         <MdDelete />
       </button>
     </div>
